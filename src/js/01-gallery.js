9c46f768d54e0a9d20f22eb9c441faafa27ca069
Vladislavv99 <vladlegur6@gmail.com>
2023-04-29 18:58:38 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const ulList = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    img =>
+      `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+   </a>
+</li>
+    `
+  )
+  .join('');
+ulList.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  spinner: true,
+  captionsData: 'alt',
+  animationSpeed: 250,
+});
