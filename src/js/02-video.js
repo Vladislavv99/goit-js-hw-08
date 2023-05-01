e0c194354f9bd3960a57ab4aa33302b16a7e6de2
Vladislavv99 <vladlegur6@gmail.com>
2023-05-01 21:46:02 +0300

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,11 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const VIDEO_CURRENT_TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
+const key = 'videoplayer-current-time';
+
 const player = new Player(iframe);
-let currentTime = JSON.parse(localStorage.getItem(VIDEO_CURRENT_TIME_KEY));
+let currentTime = JSON.parse(localStorage.getItem(key));
 
 if (currentTime) {
   player.setCurrentTime(currentTime);
@@ -14,6 +15,6 @@ player.on('play', () => console.log('Player has been started'));
 player.on(
   'timeupdate',
   throttle(data => {
-    localStorage.setItem(VIDEO_CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+    localStorage.setItem(key, JSON.stringify(data.seconds));
   }, 1000)
 );
